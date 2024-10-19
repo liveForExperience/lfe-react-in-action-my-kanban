be81a5f56b27acf8047f755363cf6c172be969f6
Chen Yue <liveforexperience@126.com>
2024-10-19 17:27:22 +0800

    feat: 完成第八课内容，新增相对时间显示功能并每分钟刷新

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { css } from "@emotion/react";
 
 const COLUMN_BG_COLORS = {
-  todo: '#C9AF97',
-  ongoing: '#FFE799',
-  done: '#C0E8BA'
-}
+  todo: "#C9AF97",
+  ongoing: "#FFE799",
+  done: "#C0E8BA",
+};
 
 const kanbanCardStyles = css`
-  margin-bottom: 1rem; 
-  padding: 0.6rem 1rem; 
-  border: 1px solid gray; 
-  border-radius: 1rem; 
-  list-style: none; 
-  background-color: rgba(255, 255, 255, 0.4); 
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
   text-align: left;
 
   &:hover {
@@ -27,6 +27,11 @@ const kanbanCardStyles = css`
 const kanbanCardTitleStyles = css`
   min-height: 3rem;
 `;
+
+const MINUTE = 60 * 1000,
+  HOUR = 60 * MINUTE,
+  DAY = 24 * HOUR,
+  UPDATE_INTERVAL = MINUTE;
 
 const KanbanBoard = ({ children }) => (
   <main
@@ -44,39 +49,41 @@ const KanbanBoard = ({ children }) => (
 
 const KanbanColumn = ({ children, bgColor, title }) => {
   return (
-    <section css={css`
-      flex: 1 1; /* 每个列根据需要缩放，且在父容器中等分 */
-      display: flex;
-      flex-direction: column;
-      border: 1px solid gray; /* 灰色边框，宽度为 1px */
-      border-radius: 1rem; /* 圆角边框，半径为 1rem */
-      background-color: ${bgColor};
+    <section
+      css={css`
+        flex: 1 1; /* 每个列根据需要缩放，且在父容器中等分 */
+        display: flex;
+        flex-direction: column;
+        border: 1px solid gray; /* 灰色边框，宽度为 1px */
+        border-radius: 1rem; /* 圆角边框，半径为 1rem */
+        background-color: ${bgColor};
 
-      & > h2 {
-        margin: 0.6rem, 0.1rem;
-        padding-bottom: 0.6rem;
-        border-bottom: 1px solid gray;
+        & > h2 {
+          margin: 0.6rem, 0.1rem;
+          padding-bottom: 0.6rem;
+          border-bottom: 1px solid gray;
 
-        & > button {
-          float: right;
-          margin-top: 0.2rem;
-          padding: 0.2rem 0.5rem;
-          border: 0;
-          border-radius: 1rem;
-          height: 1.8rem;
-          line-height: 1rem;
-          font-size: 1rem;
+          & > button {
+            float: right;
+            margin-top: 0.2rem;
+            padding: 0.2rem 0.5rem;
+            border: 0;
+            border-radius: 1rem;
+            height: 1.8rem;
+            line-height: 1rem;
+            font-size: 1rem;
+          }
         }
-      }
 
-      & > ul {
-        flex: 1;
-        flex-basis: 0;
-        margin: 1rem;
-        padding: 0;
-        overflow: auto;
-      }
-    `}>
+        & > ul {
+          flex: 1;
+          flex-basis: 0;
+          margin: 1rem;
+          padding: 0;
+          overflow: auto;
+        }
+      `}
+    >
       <h2>{title}</h2>
       <ul>{children}</ul>
     </section>
@@ -84,14 +91,42 @@ const KanbanColumn = ({ children, bgColor, title }) => {
 };
 
 const KanbanCard = ({ title, status }) => {
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status);
+      let relativeTime = "刚刚";
+      if (MINUTE <= timePassed && timePassed < HOUR) {
+        relativeTime = `${Math.ceil(timePassed / MINUTE)} 分钟前`;
+      } else if (HOUR < timePassed && timePassed < DAY) {
+        relativeTime = `${Math.ceil(timePassed / HOUR)} 小时前`;
+      } else if (DAY <= timePassed) {
+        relativeTime = `${Math.ceil(timePassed / DAY)} 天前`;
+      }
+      setDisplayTime(relativeTime);
+    };
+
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    updateDisplayTime();
+
+    return function clean() {
+      clearInterval(intervalId);
+    }
+  }, [status]);
+
   return (
     <li css={kanbanCardStyles}>
       <div css={kanbanCardTitleStyles}>{title}</div>
-      <div css={css`
+      <div
+        css={css`
           text-align: right;
           font-size: 0.8rem;
-          color: #333
-        `}>{status}</div>
+          color: #333;
+        `}
+        title={status}
+      >
+        {displayTime}
+      </div>
     </li>
   );
 };
@@ -112,12 +147,14 @@ const KanbanNewCard = ({ onSubmit }) => {
   return (
     <li css={kanbanCardStyles}>
       <h3>添加新卡片</h3>
-      <div css={css`
+      <div
+        css={css`
           ${kanbanCardTitleStyles}
           & > input[type="text"] {
             width: 80%;
           }
-        `}>
+        `}
+      >
         <input
           type="text"
           value={title}
@@ -132,19 +169,19 @@ const KanbanNewCard = ({ onSubmit }) => {
 function App() {
   const [showAdd, setShowAdd] = useState(false);
   const [todoList, setTotoList] = useState([
-    { title: "开发任务-1", status: "22-05-22 18:15" },
-    { title: "开发任务-3", status: "22-05-22 18:15" },
-    { title: "开发任务-5", status: "22-05-22 18:15" },
-    { title: "测试任务-3", status: "22-05-22 18:15" },
+    { title: "开发任务-1", status: "2024-05-22 18:15" },
+    { title: "开发任务-3", status: "2024-05-22 18:15" },
+    { title: "开发任务-5", status: "2024-05-21 18:15" },
+    { title: "测试任务-3", status: "2024-05-22 18:15" },
   ]);
   const [ongoingList, setOngoingList] = useState([
-    { title: "开发任务-4", status: "22-05-22 18:15" },
-    { title: "开发任务-6", status: "22-05-22 18:15" },
-    { title: "测试任务-2", status: "22-05-22 18:15" },
+    { title: "开发任务-4", status: "2024-8-22 18:15" },
+    { title: "开发任务-6", status: "2024-05-22 18:15" },
+    { title: "测试任务-2", status: "2024-05-22 18:15" },
   ]);
   const [doneList, setDoneList] = useState([
-    { title: "开发任务-2", status: "22-05-22 18:15" },
-    { title: "测试任务-1", status: "22-05-22 18:15" },
+    { title: "开发任务-2", status: "2024-05-22 18:15" },
+    { title: "测试任务-1", status: "2024-05-22 18:15" },
   ]);
 
   const handleAdd = (evt) => {
